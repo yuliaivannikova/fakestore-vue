ea6c18d95b8fb27590a113d39f03a372ca34d014
Yulia <yulia.godyk@gmail.com>
2026-07-27 00:18:52 +0200

    feature: add dynamic categories

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,18 +1,59 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { getCategories } from '../api/fakestore'
+import { ref, onMounted } from 'vue'
+import type { Category } from '../types/index'
+
+const categories = ref<Category[]>([])
+
+onMounted(async () => {
+  categories.value = await getCategories()
+})
+</script>
 
 <template>
   <header class="app-header">
-    <h1>App Header</h1>
+    <h1>FakeStore</h1>
     <nav>
       <RouterLink to="/">Home</RouterLink>
+      <RouterLink v-for="category in categories" :key="category" :to="`/category/${category}`">{{
+        category
+      }}</RouterLink>
     </nav>
   </header>
 </template>
 
 <style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+header h1 {
+  font-size: 1.5rem;
+  font-weight: 600;
+  text-transform: capitalize;
+}
+
 .app-header {
   background-color: var(--color-bg);
   padding: 1rem;
   border-bottom: 1px solid var(--color-border);
+}
+
+.app-header nav {
+  display: flex;
+  gap: 1rem;
+}
+
+.app-header nav a {
+  text-decoration: none;
+  color: var(--color-text);
+  text-transform: capitalize;
+  transition: color 0.3s ease;
+}
+
+.app-header nav a:hover {
+  color: var(--color-primary);
 }
 </style>
